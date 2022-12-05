fc468c98cf365f6679e0434f255f616b81651f59
c1012893 <R.R.webb2@ncl.ac.uk>
2022-12-05 16:51:10 +0000

    password must be between 6-12 characters

--- users/forms.py
+++ users/forms.py
@@ -1,13 +1,13 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, PasswordField
-from wtforms.validators import DataRequired, Email
+from wtforms import StringField, SubmitField, PasswordField, EmailField
+from wtforms.validators import DataRequired, Email, Length
 
 
 class RegisterForm(FlaskForm):
-    email = StringField(validators=[DataRequired(), Email()])
+    email = EmailField(validators=[DataRequired(), Email()])
     firstname = StringField(validators=[DataRequired()])
     lastname = StringField(validators=[DataRequired()])
     phone = StringField(validators=[DataRequired()])
-    password = PasswordField(validators=[DataRequired()])
+    password = PasswordField(validators=[Length(min=6, max=12)])
     confirm_password = PasswordField(validators=[DataRequired()])
     submit = SubmitField(validators=[DataRequired()])
