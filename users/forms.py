449bc78b73281d544c553139e531ea3e468ff93c
c1012893 <R.R.webb2@ncl.ac.uk>
2022-12-06 14:41:49 +0000

    contain at least 1 special character, 1 upper case, 1 lower case and 1 digit

--- users/forms.py
+++ users/forms.py
@@ -3,22 +3,26 @@ from wtforms import StringField, SubmitField, PasswordField, EmailField
 from wtforms.validators import DataRequired, Email, Length, EqualTo, ValidationError
 import re
 
+def character_check(form, field):
+    excluded_chars = "!£$%^&*()-+@~#?"
+    for char in field.data:
+        if char in excluded_chars:
+      raise ValidationError(f"Character{char} is not allowed.")
 
-def validate_data(data_field):
-    p = re.compile("regex goes here")
-    if not p.match(data_field.data):
-        raise ValidationError("error message goes here")
-
+ def validate_data(form, password):
+    p = re.compiler(r'(?=.*\d)(?=.*[a-zA-Z])(?=.*[+!"£$%^&*()-=[{]};:@#~/?.>,<\|])')
+    if p.match(password.data):
+        return
+    raise ValidationError("Must contain at least 1 digit, contain at least 1 lowercase, 1 upper case character  and at least 1 special character")
 
 class RegisterForm(FlaskForm):
     email = EmailField(validators=[DataRequired(), Email()])
     firstname = StringField(validators=[DataRequired()])
     lastname = StringField(validators=[DataRequired()])
     phone = StringField(validators=[DataRequired()])
-    password = PasswordField(validators=[Length(min=6, max=12)])
-    confirm_password = PasswordField(validators=[EqualTo('password', message='Both password fields must be equal!')])
+    password = PasswordField(validators=[Length(min=6, max=12), character_check(), DataRequired()])
+    confirm_password = PasswordField(validators=[EqualTo('password', message='Both password fields must be equal!'), character_check(), DataRequired()])
     submit = SubmitField(validators=[DataRequired()])
 
- myString = StringField(validators=[])
 
-r'(?=.*\d)(?=.*[a-zA-Z])(?=.*[+*.${}]'
+
