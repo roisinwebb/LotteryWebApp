149bfc23e6b02e6e6d0492234066b082c9610fe8
c1012893 <R.R.webb2@ncl.ac.uk>
2023-01-04 14:39:04 +0000

    contain at least 1 special character, 1 upper case, 1 lower case and 1 digit

--- app.py
+++ app.py
@@ -34,6 +34,26 @@ app.register_blueprint(admin_blueprint)
 app.register_blueprint(lottery_blueprint)
 
 
+# error handling
+@app.errorhandler(404)
+def bad_request(error):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(403)
+def forbidden(error):
+    return render_template('403.html'), 403
+
+
+@app.errorhandler(500)
+def internal_error(error):
+    return render_template('500.html'), 500
+
+
+@app.errorhandler(503)
+def service_unavailable(error):
+    return render_template('503.html'), 503
+
+
 if __name__ == "__main__":
     app.run()
-
--- users/forms.py
+++ users/forms.py
@@ -4,25 +4,49 @@ from wtforms.validators import DataRequired, Email, Length, EqualTo, ValidationE
 import re
 
 
+# FORMS
+# characters to be excluded in first name and last name
+def character_check(form, field):
+    excluded_chars = "* ? ! ' ^ + % & / ( ) = } ] [ { $ # @ < >"
+    for char in field.data:
+        if char in excluded_chars:
+            raise ValidationError(f"Character {char} is not allowed.")
+
+
+# password must contain at least 1 uppercase, 1 lowercase, 1 digit and 1 special character
+def validate_data(form, password):
+    p = re.compile(r'(?=.*\d)(?=.*[a-zA-Z])(?=.*\W)')
+    if not p.match(password.data):
+        raise ValidationError("Must contain at least 1 digit, 1 lowercase, 1 uppercase and a special character ")
+
+
+# phone number must be in the format XXXX-XXX-XXXX
+def validate_number(form, phone):
+    p = re.compile(r'(\d{4})-(\d{3})-(\d{4})')
+    if not p.match(phone.data):
+        raise ValidationError("Must be in the format XXXX-XXX-XXXX")
+
+
+# registration form for website
 class RegisterForm(FlaskForm):
-
-    def validate_data(self, password):
-        p = re.compile(r'(?=.*\d)(?=.*[a-zA-Z])(?=.*[+!"£$%^&*();:@#~/?.>,<])')
-        if not p.match(password.data):
-            raise ValidationError("Must contain at least 1 digit, contain at least 1 lowercase, "
-                                  "1 upper case character  and at least 1 special character")
-
-    def character_check(self, field):
-        excluded_chars = "!£$%^&*()-+@~#?"
-        for char in field.data:
-            if char in excluded_chars:
-                raise ValidationError(f"Character {char} is not allowed.")
-
+    # email format is needed and data must be added
     email = EmailField(validators=[DataRequired(), Email()])
-    firstname = StringField(validators=[DataRequired()])
-    lastname = StringField(validators=[DataRequired()])
-    phone = StringField(validators=[DataRequired(), "([0-9]{4})-([0-9]{3})-([0-9]{4})"])
-    password = PasswordField(validators=[DataRequired(), character_check, Length(min=6, max=12)])
-    confirm_password = PasswordField(validators=[DataRequired(), character_check,
-                                                 EqualTo('password', message='Both password fields must be equal!')])
-    submit = SubmitField(validators=[DataRequired()])
+    # data must be added and character check for no special characters
+    firstname = StringField(validators=[DataRequired(), character_check])
+    # data must be added and character check for no special characters
+    lastname = StringField(validators=[DataRequired(), character_check])
+    # data must be added and phone number format
+    phone = StringField(validators=[DataRequired()])
+    # password must be added and must be between 6 and 12 characters
+    password = PasswordField(validators=[DataRequired(), validate_data, Length(min=6, max=12)])
+    # password must be added and both passwords must match
+    confirm_password = PasswordField(validators=[DataRequired(), validate_data, EqualTo('password',
+                                                                    message='Both password ' 'fields must be equal!')])
+    submit = SubmitField()
+
+
+# login form for website
+class LoginForm(FlaskForm):
+    username = StringField(validators=[DataRequired(), Email()])
+    password = PasswordField(validators=[DataRequired()])
+    submit = SubmitField()
